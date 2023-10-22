c7705eb46877ff061cd5c455af20afdda11d686e
keiwv <141773283+keiwv@users.noreply.github.com>
2023-10-22 14:44:42 -0700

    modified:   Practicas/Practica6_ApuntadoresyArgumentos_PerezVentura_BrayanIvan/Practica6_ApuntadoresyArgumentos_PerezVentura_BrayanIvan.c

--- Practicas/Practica6_ApuntadoresyArgumentos_PerezVentura_BrayanIvan/Practica6_ApuntadoresyArgumentos_PerezVentura_BrayanIvan.c
+++ Practicas/Practica6_ApuntadoresyArgumentos_PerezVentura_BrayanIvan/Practica6_ApuntadoresyArgumentos_PerezVentura_BrayanIvan.c
@@ -1,10 +1,24 @@
 /*
+Brayan Ivan Perez Ventura - 372781
+
+Created code: October 11th 2023 / Modified code: Octuber 22th 2023
+
+DESCRIPTION
+In this program, we will implemented a program where it will be working with pointers.
+It will display a menu where:
+    1.- Sum the elements in an array: We will fill an array and using pointers, we will sum them.
+    2.- Copy array: Using pointers, copy an array from a different array.
+    3.- Concatenate arrays: Using pointers, we will concatenate arrays.
+    4.- Comparation: We will comparate arrays using pointers
+    5.- Find the max element. Using pointers, we need to find the max element in an array using pointers.
+
+#NOTE: For this code, it's necessary the personal library called "Frijoles.h".
+
 
 */
 
 //**** LIBRARIES ***
-#include <stdio.h>
-#include <stdlib.h>
+
 #include "Frijoles.h"
 
 //**** PROTOTYPE FUNCTIONS ****
@@ -24,6 +38,24 @@ int main()
     return 0;
 }
 //**** PROTOTYPE DEVELOPMENT FUNCTIONS ****
+/*
+Function: menu
+Description: Implements a menu to perform various operations on arrays.
+Parameters: None.
+Return Value: None.
+Variables:
+- op: Integer to store the user's menu choice.
+- i: Loop variable.
+- size: Size of the main integer array.
+- nSize: Size of the second integer array.
+- array: Main integer array.
+- copyyArray: Integer array used for copying elements.
+- newArray: Second integer array.
+- finalArray: Integer array to store the concatenated result.
+- compArray: Integer array for array comparison.
+- compReturn: Integer to store the result of array comparison.
+- maxNumber: Maximum number in the array.
+*/
 void menu()
 {
     int op, i, size, nSize;
@@ -83,11 +115,20 @@ void menu()
             maxNumber = findElementMax(size, array);
             printf("El numero mayor encontrado es: %d\n", maxNumber);
             system("PAUSE");
+            break;
         }
     } while (op != 0);
 }
 
 //******************
+/*
+Function: msge_menu
+Description: Displays a menu to the user, asks them to select an option, and validates the user's input within a specified range.
+Parameters: None.
+Return Value: The value of the user's input (an integer representing the selected option).
+Variables:
+- op: An integer to store the user's choice.
+*/
 int msge_menu()
 {
     int op;
@@ -102,6 +143,18 @@ int msge_menu()
 }
 
 //****************
+/*
+Function: sumArray
+Description: Calculates the sum of elements in an integer array.
+Parameters:
+- size: The size of the array.
+- array: Pointer to the integer array.
+Return Value: None.
+Variables:
+- sum: Integer to store the sum of array elements.
+- i: Loop variable.
+- ptr: Pointer to iterate through the array.
+*/
 void sumArray(int size, int *array)
 {
     int sum, i;
@@ -112,11 +165,23 @@ void sumArray(int size, int *array)
         sum += *ptr;
         ptr++;
     }
-    printf("%d\n", sum);
+    printf("La suma del vector es: %d\n", sum);
     system("PAUSE");
 }
 
-//*****************
+/*
+Function: copyArrayy
+Description: Copies the elements from one integer array to another.
+Parameters:
+- size: The size of the arrays.
+- array: Pointer to the source array.
+- copyArray: Pointer to the destination array.
+Return Value: None.
+Variables:
+- i: Loop variable.
+- ptr1: Pointer to iterate through the source array.
+- ptr2: Pointer to iterate through the destination array.
+*/
 void copyArrayy(int size, int *array, int *copyArray)
 {
     int i;
@@ -131,7 +196,23 @@ void copyArrayy(int size, int *array, int *copyArray)
     system("PAUSE");
 }
 
-//************
+/*
+Function: concatenateArrays
+Description: Concatenates two integer arrays into a third array.
+Parameters:
+- size: The size of the first array.
+- array: Pointer to the first array.
+- newArray: Pointer to the second array.
+- finalArray: Pointer to the concatenated array.
+- nSize: The size of the second array.
+Return Value: None.
+Variables:
+- i: Loop variable.
+- ptr1: Pointer to iterate through the first array.
+- ptr2: Pointer to iterate through the second array.
+- ptr3: Pointer to iterate through the concatenated array.
+- fSize: The size of the concatenated array.
+*/
 void concatenateArrays(int size, int *array, int *newArray, int *finalArray, int nSize)
 {
     int i;
@@ -156,6 +237,19 @@ void concatenateArrays(int size, int *array, int *newArray, int *finalArray, int
     }
 }
 
+/*
+Function: comparationArray
+Description: Compares two integer arrays for equality.
+Parameters:
+- size: The size of the arrays.
+- array: Pointer to the first array.
+- compArray: Pointer to the second array.
+Return Value: 1 if the arrays are equal, 0 otherwise.
+Variables:
+- i: Loop variable.
+- ptr1: Pointer to iterate through the first array.
+- ptr2: Pointer to iterate through the second array.
+*/
 int comparationArray(int size, int *array, int *compArray)
 {
     int i;
@@ -173,6 +267,17 @@ int comparationArray(int size, int *array, int *compArray)
     return 1;
 }
 
+/*
+Function: findElementMax
+Description: Finds the maximum element in an integer array.
+Parameters:
+- size: The size of the array.
+- array: Pointer to the integer array.
+Return Value: The maximum element in the array.
+Variables:
+- max: Integer to store the maximum element.
+- ptr: Pointer to iterate through the array.
+*/
 int findElementMax(int size, int *array)
 {
     int max = *array;
@@ -188,11 +293,23 @@ int findElementMax(int size, int *array)
     return max;
 }
 
+/*
+Function: fillArray
+Description: Fills an integer array with random numbers within a given range.
+Parameters:
+- size: The size of the array.
+- array: Integer array to be filled.
+- ri: Minimum random value.
+- rf: Maximum random value.
+Return Value: None.
+Variables:
+- i: Loop variable.
+*/
 void fillArray(int size, int array[], int ri, int rf)
 {
     int i;
-    for(i = 0; i < size; i++)
+    for (i = 0; i < size; i++)
     {
-        array[i] = numRandom(ri,rf);
+        array[i] = numRandom(ri, rf); // Note: The function numRandom is used, but it's not defined in the provided code.
     }
 }
